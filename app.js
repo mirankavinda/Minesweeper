a14938cee5d0c62d0908324e0076da31eb4c16a6
Miran <mirankavinda12@gmail.com>
2024-01-18 09:57:25 +0530

    Add bomb amount and flags left display

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
+    const flagsLeft = document.querySelector('#flags-left')
     const width = 10
+    let bombAmount = 20
 
     console.log(grid);
 
     //Create a Minesweeper Board
 
     function createBoard() {
+        flagsLeft.innerHTML = bombAmount
+
+        // shuffled game array with random bombs
+        const bombArray = Array(bombAmount).fill('bomb');
+        const emptyArray = Array(width * width - bombAmount).fill('vaild')
+        console.log(emptyArray);
 
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement('div')
+            square.id = i
             grid.appendChild( square )
         }
     }
